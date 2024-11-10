292ead8bb66b148d00e1b1a63fc898532c85627c
Marina Chen <chen.marina.vm10@gmail.com>
2024-11-10 21:53:13 +0500

    Add log.all()

--- src/test/java/specs/WebAPIMethodsSpecs.java
+++ src/test/java/specs/WebAPIMethodsSpecs.java
@@ -6,30 +6,24 @@ import io.restassured.specification.ResponseSpecification;
 
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
-import static io.restassured.filter.log.LogDetail.BODY;
-import static io.restassured.filter.log.LogDetail.STATUS;
+import static io.restassured.filter.log.LogDetail.*;
 import static io.restassured.http.ContentType.JSON;
 
 public class WebAPIMethodsSpecs {
     public static RequestSpecification requestSpecs = with()
             .filter(withCustomTemplates())
-            .log().uri()
-            .log().body()
-            .log().headers()
+            .log().all()
             .contentType(JSON);
     public static ResponseSpecification responseSpecs200 = new ResponseSpecBuilder()
             .expectStatusCode(200)
-            .log(STATUS)
-            .log(BODY)
+            .log(ALL)
             .build();
     public static ResponseSpecification responseSpecs201 = new ResponseSpecBuilder()
             .expectStatusCode(201)
-            .log(STATUS)
-            .log(BODY)
+            .log(ALL)
             .build();
     public static ResponseSpecification responseSpecs204 = new ResponseSpecBuilder()
             .expectStatusCode(204)
-            .log(STATUS)
-            .log(BODY)
+            .log(ALL)
             .build();
 }
